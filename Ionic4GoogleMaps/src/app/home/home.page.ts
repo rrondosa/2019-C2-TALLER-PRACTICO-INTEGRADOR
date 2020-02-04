8535b6234f97a7b68ecf3ccc04d742cbe3d7d2f0
Rondo Sanchez <rrondo@indracompany.com>
2020-02-04 11:31:31 -0300

    proyecto ionic4 mapa con geolocalizacion

---
+++ Ionic4GoogleMaps/src/app/home/home.page.ts
@@ -0,0 +1,96 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
+ 
+declare var google;
+ 
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+ 
+  @ViewChild('map' ,{static: false}) mapElement: ElementRef;
+  map: any;
+  address:string;
+ 
+  constructor(
+    private geolocation: Geolocation,
+    private nativeGeocoder: NativeGeocoder) {
+  }
+ 
+ 
+  ngOnInit() {
+    this.loadMap();
+  }
+ 
+  loadMap() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      console.log("pto1", resp);
+      let latLng = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+      let mapOptions = {
+        center: latLng,
+        zoom: 18,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+ 
+      this.getAddressFromCoords(resp.coords.latitude, resp.coords.longitude);
+ 
+      this.map = new google.maps.Map(document.getElementById('map') , mapOptions);
+ 
+      this.map.addListener('tilesloaded', () => {
+        console.log('accuracy',this.map);
+        this.getAddressFromCoords(this.map.center.lat(), this.map.center.lng())
+      });
+
+      google.maps.event
+        .addListenerOnce(this.map, 'idle', () => {
+          // loading.dismiss();
+          this.addMaker(latLng.lat, latLng.lng);
+      });
+ 
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+  }
+
+  private addMaker(lat: number, lng: number) {
+    const marker = new google.maps.Marker({
+      position: { lat, lng },
+      map: this.map,
+      title: 'Hello World!'
+    });
+  }
+ 
+  getAddressFromCoords(lattitude, longitude) {
+    console.log("getAddressFromCoords "+lattitude+" "+longitude);
+    let options: NativeGeocoderOptions = {
+      useLocale: true,
+      maxResults: 5
+    };
+ 
+    this.nativeGeocoder.reverseGeocode(lattitude, longitude, options)
+      .then((result: NativeGeocoderResult[]) => {
+        this.address = "";
+        let responseAddress = [];
+        for (let [key, value] of Object.entries(result[0])) {
+          if(value.length>0)
+          responseAddress.push(value);
+ 
+        }
+        responseAddress.reverse();
+        for (let value of responseAddress) {
+          this.address += value+", ";
+        }
+        this.address = this.address.slice(0, -2);
+      })
+      .catch((error: any) =>{ 
+        this.address = "Address Not Available!";
+      });
+ 
+  }
+ 
+ 
+}
